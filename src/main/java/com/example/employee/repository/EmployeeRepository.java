ba4d552382c68ab1cf822574c95ed505ff894513
tongsq19 <tongsq14@mails.tsinghua.edu.cn>
2019-04-17 20:16:24 +0800

    refractor variable name in test 2 and test 3

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -13,15 +13,15 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Employee findFirstByName(String name);
 
     //2.找出Employee表中第一个姓名包含`*`字符并且薪资大于*的雇员个人信息
-    Employee findFirstByNameLikeAndSalaryIsGreaterThan(String n, int i);
-
+    Employee findFirstByNameLikeAndSalaryIsGreaterThan(String characters, int salary);
 
     //3.找出一个薪资最高且公司ID是*的雇员以及该雇员的姓名
-    Employee findFirstByCompanyIdOrderBySalaryDesc(int i);
+    Employee findFirstByCompanyIdOrderBySalaryDesc(int companyId);
 
     //4.实现对Employee的分页查询，每页两个数据
 
     //5.查找**的所在的公司的公司名称
+    String findCompanyNameByName(String name);
 
     //6.将*的名字改成*,输出这次修改影响的行数
 
